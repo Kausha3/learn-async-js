8178b604fa667ee225701ef57f11df56ab371fc2
Kausha3 <133591404+Kausha3@users.noreply.github.com>
2024-03-02 15:07:47 -0500

    Update promise-sum-2darray.js

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,5 +1,31 @@
+function sumArray(arr) {
+    return arr.reduce((sum, value) => sum + value, 0);
+}
+
+function sum2DArrayConcurrently(array2D) {
+    const rowSums = array2D.map(row => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                const rowSum = sumArray(row);
+                resolve(rowSum);
+            }, 0);
+        });
+    });
+
+    // Use Promise.all to wait for all row sums to be computed
+    return Promise.all(rowSums)
+        .then(sums => {
+            // Once all Promises resolve, sums will be an array of the sum of each row
+            // Sum up these values to get the total
+            return sumArray(sums);
+        });
+}
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
 ];
+
+sum2DArrayConcurrently(array2D)
+    .then(totalSum => console.log(`Total Sum: ${totalSum}`))
+    .catch(error => console.error(`Error: ${error}`));
