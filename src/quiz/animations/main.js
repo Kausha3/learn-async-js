b6e553b164293dc1571070964a91d14cfd53dea8
Kausha3 <133591404+Kausha3@users.noreply.github.com>
2024-03-06 22:04:57 -0500

    Update main.js

--- src/quiz/animations/main.js
+++ src/quiz/animations/main.js
@@ -1,42 +1,29 @@
-const aliceTumbling = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
-  ];
-  
-  const aliceTiming = {
-    duration: 2000,
-    iterations: 1,
-    fill: 'forwards'
-  }
-  
-  const alice1 = document.querySelector("#alice1");
-  const alice2 = document.querySelector("#alice2");
-  const alice3 = document.querySelector("#alice3");
+async function animateAliceSequence() {
+    const aliceTumbling = [
+        { transform: 'rotate(0) scale(1)' },
+        { transform: 'rotate(360deg) scale(0)' }
+    ];
 
-  alice1
-    .animate(aliceTumbling, aliceTiming)
-    .finished
-    .then((res) => {
-        console.log(res);
-        alice2
-            .animate(aliceTumbling, aliceTiming)
-            .finished
-            .then((res) => {
-                console.log(res);
-                alice3.animate(aliceTumbling, aliceTiming);
-            })
-    });
+    const aliceTiming = {
+        duration: 2000,
+        iterations: 1,
+        fill: 'forwards'
+    };
 
-  // Promise chain  
-  // alice1.animate(aliceTumbling, aliceTiming).finished  
-  //   .then(() => {
-  //       return alice2
-  //               .animate(aliceTumbling, aliceTiming)
-  //               .finished;     
-  //   })
-  //   .then(() => {
-  //     return alice3
-  //             .animate(aliceTumbling, aliceTiming)
-  //             .finished;
-  //   })
-  //   .catch((err) => alert(`Error when promising ... ${err.message}`));
+    const alice1 = document.querySelector("#alice1");
+    const alice2 = document.querySelector("#alice2");
+    const alice3 = document.querySelector("#alice3");
+
+    try {
+        await alice1.animate(aliceTumbling, aliceTiming).finished;
+        console.log("Alice1 animation finished");
+        await alice2.animate(aliceTumbling, aliceTiming).finished;
+        console.log("Alice2 animation finished");
+        await alice3.animate(aliceTumbling, aliceTiming).finished;
+        console.log("Alice3 animation finished");
+    } catch (err) {
+        console.error(`Error when animating: ${err.message}`);
+    }
+}
+
+animateAliceSequence();
